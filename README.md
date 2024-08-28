358433c379cc47a2ad8c8f029050768679c80931
Yasin Gole <yasingole98@gmail.com>
2024-08-28 19:01:37 +0100

    grid auto sizing added

---
+++ README.md
@@ -0,0 +1,12 @@
+Etch and Sketch is a simple web-based drawing application that allows users to create a grid of squares, where they can "draw" by hovering over the squares with their mouse. Users can select the size of the grid, ranging from 1x1 to 100x100 squares, and the grid will automatically adjust to fit within the container. This project is built using HTML, CSS, and JavaScript.
+
+Features
+
+Dynamic Grid Creation: Users can choose a grid size between 1 and 100 squares. The grid is dynamically generated based on the input.
+Interactive Drawing: Hovering over the grid squares changes their color, simulating a drawing effect.
+Input Validation: Ensures that users enter a valid number between 1 and 100. If the input is invalid, an error message is displayed.
+Technologies Used
+
+HTML: Provides the structure of the web page.
+CSS: Styles the page, including the layout of the grid and input elements.
+JavaScript: Handles the logic for grid creation, input validation, and the drawing effect.
--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 console.log("Hi")
+
+//Input and Error 
+const numberInput= document.getElementById("number-input");
+const errorMessage = document.getElementById("error-message");
+
+//Listener for input
+numberInput.addEventListener("input", () => {
+    const numberInputValue= parseInt(numberInput.value, 10);
+
+    //Check is number
+    if (isNaN(numberInputValue) || numberInputValue < 1 || numberInputValue > 100) {
+        errorMessage.style.display= "block";
+        document.getElementById("container").innerHTML = "";
+    } else {
+        errorMessage.style.display = 'none';
+
+        //Creating the squares in the grid
+        const container= document.getElementById("container");
+
+        container.innerHTML= "";
+
+        const containerSize= 640;
+        const gridSize= containerSize/numberInputValue;
+
+        for (let i=0; i < (numberInputValue*numberInputValue); i++) {
+        const square= document.createElement("div");
+        square.className= "grid";
+        square.style.width= `${gridSize}px`;
+        square.style.height= `${gridSize}px`;
+        container.appendChild(square)
+        }
+    }
+
+    //Adding hovering over grid
+    const squares= document.querySelectorAll(".grid");
+    squares.forEach(square => {
+    
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor= "grey";
+        })
+    })
+})
+
+
+
+
+
